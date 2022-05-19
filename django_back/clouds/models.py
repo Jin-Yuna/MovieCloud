d311269b547de18e538fd6f210f82e1e4baf8c14
le2sk <tmfrl935@naver.com>
2022-05-19 21:59:10 +0900

    시리얼라이즈ing

--- django_back/clouds/models.py
+++ django_back/clouds/models.py
@@ -3,14 +3,14 @@ from django.conf import settings
 from movies.models import Movie
 
 class Drop(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='drops')
     movie = models.ForeignKey(Movie, on_delete=models.SET_DEFAULT, default=0)
     title = models.CharField(max_length=100)
     content = models.TextField()
     user_vote = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_drops')
 
 
 class Comment(models.Model):
