063af632c1a7ddba179dd1bcf40525ce9462dec3
le2sk <tmfrl935@naver.com>
2022-05-19 20:48:25 +0900

    모델링

---
+++ django_back/clouds/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from ..movies.models import Movie 
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    movie = models.ForeignKey(Movie, on_delete=models.SET_DEFAULT, default='영화 데이터가 없습니다.')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user_vote = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
---
+++ django_back/movies/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=20)
+    genres = models.CharField(max_length=100)
+    release_date = models.DateField(null=True)
+    poster_path = models.CharField(max_length=150, null=True)
+    overview = models.TextField()
+    tagline = models.TextField(null=True)
+    adult = models.BooleanField(null=True)
+    backdrop_path = models.CharField(max_length=150, null=True)
+    original_language = models.CharField(max_length=5)
+    original_title = models.CharField(max_length=30)
+    popularity = models.FloatField(null=True)
+    production_countries = models.CharField(max_length=20)
+    runtime = models.IntegerField(null=True)
+    spoken_languages = models.CharField(max_length=10)
+    vote_average = models.FloatField(null=True)
+    vote_count = models.IntegerField(null=True)
+    production_companies = models.TextField(null=True)
+    video_key = models.CharField(max_length=40, null=True)
