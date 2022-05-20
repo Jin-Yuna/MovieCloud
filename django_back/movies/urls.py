a018c594e44394ad1f101e377b8c11915666ea3d
le2sk <tmfrl935@naver.com>
2022-05-21 02:41:28 +0900

    Drop생성(유저정보넘겨주기못하는중)

--- django_back/movies/urls.py
+++ django_back/movies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('get_movie_title/', views.get_movie_title)   
 ]
---
+++ final-pjt-front/src/views/DropListView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <DropCreate />
+    <h1>게시판</h1>
+    <div v-for="drop in drops" :key="drop.id">
+      {{ drop }} -->
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import DropCreate from '@/components/DropCreate.vue'
+
+export default {
+  name: 'DropList',
+  components: {
+    DropCreate,
+  },
+  data() {
+    return {
+      drops : []
+    }
+  },
+  created() {
+    const config = {
+      headers: {
+        Authorization: `Token ${this.$store.state.token}`
+      }
+    }
+    axios.get('http://127.0.0.1:8000/drops/', config)
+      .then(response => {
+        this.drops = response.data
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
