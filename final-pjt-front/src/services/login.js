2054e303e02bf576ed9bd640fa43fbe022bd84b3
YUNA <84631075+Jin-Yuna@users.noreply.github.com>
2022-05-21 00:42:15 +0900

    소셜 로그인의 백 연동을 어떤 식으로 해야할지에 대한 고민과 프론트 코드 다듬기

--- final-pjt-front/src/services/login.js
+++ final-pjt-front/src/services/login.js
@@ -6,6 +6,7 @@ const kakaoHeader = {
     'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
 };
 
+// 토큰 받아오기
 const getKakaoToken = async (code) => {
     console.log('loginWithKakao');
     try {
@@ -24,7 +25,7 @@ const getKakaoToken = async (code) => {
         const result = await axios.post('https://kauth.kakao.com/oauth/token', queryString, { headers: kakaoHeader });
         // const result = await axios.post(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${data.client_id}&redirect_uri=${data.redirect_uri}&code=${data.code}`);
         // console.log('카카오 토큰', queryString);
-        console.log(result)
+        console.log(result.data.access_token)
         const token_header = {
             'Token': result.data.access_token
         }
@@ -37,6 +38,7 @@ const getKakaoToken = async (code) => {
     }
 };
 
+// 유저 정보 가져오기
 const getKakaoUserInfo = async () => {
     let data = '';
     await window.Kakao.API.request({
--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -7,16 +7,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    id: '',
-    userName: '',
-    platform: '',
   },
   mutations: {
-    setUser(state, payload) {
-      state.id = payload.id
-      state.userName = payload.name
-      state.platform = payload.platform
-    }
   },
   modules: {  
     accounts,
