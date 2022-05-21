d911dbf77476c3702759d98dc1311a59a4b54aea
YUNA <84631075+Jin-Yuna@users.noreply.github.com>
2022-05-22 03:01:27 +0900

    마커 살림

--- final-pjt-front/src/components/Map.vue
+++ final-pjt-front/src/components/Map.vue
@@ -4,16 +4,29 @@
 </template>
 
 <script>
+let kakao = window.kakao
+
 export default {
   name: "KakaoMap",
   data() {
     return {
       map: null,
       markerPositions: [
-        { title: '현재 위치', 
-        latlng: new window.kakao.maps.LatLng(this.$store.state.latitude, this.$store.state.longitude) },
+        [ this.$store.state.latitude, this.$store.state.longitude ],
       ],
       markers: [],
+    }
+  },
+  // 지도 불러오기
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    }
+    else {
+      const script = document.createElement("script");
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src ='https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=fcf79d7950891e2a0bcbec183a0187dd'
+      document.head.appendChild(script)
     }
   },
   methods: {
@@ -21,62 +34,49 @@ export default {
     initMap() {
       const container = document.getElementById("map")
       const options = {
-        center: new window.kakao.maps.LatLng(this.$store.state.latitude, this.$store.state.longitude),
+        center: new kakao.maps.LatLng(this.$store.state.latitude, this.$store.state.longitude),
         level: 5,
       }
-      this.map = new window.kakao.maps.Map(container, options)
+      this.map = new kakao.maps.Map(container, options)
+      this.displayMarker(this.markerPositions)
       },
     // 마커 표시
-    displayMarker(positions) {
+    displayMarker(markerPositions) {
     // 현재 표시되어 있는 marker들이 있으면 marker에 등록된 map 없애주기
     if (this.markers.length > 0) {
       this.markers.forEach((marker) => marker.setMap(null));
     }
 
-    // 마커 이미지 커스터마이징
-    const imgSrc = require('@/assets/marker.png')
-    const imgSize = new window.kakao.maps.Size(24, 35)
-    const markerImage = new window.kakao.maps.MarkerImage(imgSrc, imgSize)
+    const positions = markerPositions.map(
+      (position) => new kakao.maps.LatLng(...position)
+    );
     
     // 마커 표시하기
-    positions.forEach((position) => {
-      const marker = new window.kakao.maps.Marker({
-        map: this.map,
-        position: position.latlng,
-        title: position.title,  // 마우스 오버시 표시할 제목
-        image: markerImage,
-      })
-      this.markers.push(marker)
-    })
-      // 지도 이동
-      const bounds = positions.reduce(
-        (bounds, position) => bounds.extend(position.latlng),
-        new window.kakao.maps.LatLngBounds()
-      );
+    if (positions.length > 0) {
+        this.markers = positions.map(
+          (position) =>
+            new kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
+        );
 
-      this.map.setBounds(bounds);
-    }
+        const bounds = positions.reduce(
+          (bounds, latlng) => bounds.extend(latlng),
+          new kakao.maps.LatLngBounds()
+        );
+
+        this.map.setBounds(bounds);
+      }
     },
-    // 지도 불러오기
-    mounted() {
-      if (!window.kakao || !window.kakao.maps) {
-        const script = document.createElement("script");
-        script.src ='https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=fcf79d7950891e2a0bcbec183a0187dd'
-        script.addEventListener("load", () => {
-          window.kakao.maps.load(this.initMap)
-        })
-        document.head.appendChild(script)
-      } else {
-        this.initMap()
-     }
-  },
+  }
 }
 </script>
 
 <style>
 #map {
-  width: 400px;
-  height: 400px;
+  width: 100%;
+  height: 600px;
 }
 
 </style>
