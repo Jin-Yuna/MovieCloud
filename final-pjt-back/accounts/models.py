4442157f6bfeb65f6b541cc07a4b9a735c9fe4c2
YUNA <84631075+Jin-Yuna@users.noreply.github.com>
2022-05-23 02:52:01 +0900

    카카오 서버 통신 중 => 서버로 토큰 넘겨주고 유저 정보 받는 것 성공 => 모델 변경 전 커밋

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    # movie_id = models.textField()
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
---
+++ final-pjt-back/accounts/views.py
@@ -0,0 +1,43 @@
+from django.http import JsonResponse
+import requests
+from .models import User
+
+def kakaoLogin(request):
+    
+    auth_code = request.GET.get('code')
+    kakao_token_api = 'https://kauth.kakao.com/oauth/token'
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': '683d19aa3f66f6c7d4ca3b08f6f139ed',
+        'redirect_uri': 'http://127.0.0.1:8000/kakaoLogin',
+        'code': auth_code,
+    }
+    token_response = requests.post(kakao_token_api, data=data)
+    access_token = token_response.json().get('access_token')
+    user_info_response = requests.get('https://kapi.kakao.com/v2/user/me',
+    headers={"Authorization": f'Bearer ${access_token}'})
+    user_info_response = user_info_response.json()
+
+    # 이미 있는 회원인지 검사
+    is_exist = False
+    users = User.objects.all()
+    for user in users:
+        if user.id == user_info_response['id']:
+            is_exist = True
+            break
+    
+    if is_exist == False:
+        print(user_info_response['id'])
+        # User.objects.create(
+        #     id = user_info_response['id'],
+        #     is_superuser = 0,
+        #     is_staff = 0,
+        #     is_active = 1, 
+        #     date_joined = user_info_response['connected_at'],
+        #     username = user_info_response['properties']['nickname']
+        # )
+
+    print(User.objects.filter(pk=user_info_response['id']))
+
+    return JsonResponse({'user_info': user_info_response})
+
