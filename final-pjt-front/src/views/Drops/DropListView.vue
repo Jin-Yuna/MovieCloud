ac432c2909aeeb6c4b55c8ef85985dee0b5742fd
le2sk <tmfrl935@naver.com>
2022-05-26 18:21:42 +0900

    게시판

--- final-pjt-front/src/views/Drops/DropListView.vue
+++ final-pjt-front/src/views/Drops/DropListView.vue
@@ -1,23 +1,49 @@
 <template>
-  <div>
-    <h1>게시판</h1>
-    <DropCard
-      v-for="drop in drops"
-      :key="drop.id"
-      :drop="drop"
-    />
+  <div class="mymargintop">
+    <v-flex class="justify-center">
+      <h1 class="my_bord_text">DROPS</h1>
+    </v-flex>
+      <v-container class="mymargintop">
+        <v-row justify="center">
+          <v-col
+            v-for="drop in drops"
+            :key="drop.id"
+            cols="auto"
+          >
+            <v-col
+              :elevation="drops.length - 1"
+              height="300"
+              width="200"
+            >
+              <v-row
+                class="fill-height"
+                align="center"
+                justify="center"
+              >
+              <RealDropCard 
+                :drop="drop"
+                class="mr-5" 
+              />
+              </v-row>
+            </v-col>
+          </v-col>
+        </v-row>
+      </v-container>
+    <div class="ocean">
+      <div class="wave"></div>
+      <div class="wave"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import drf from '@/api/drf'
-import DropCard from '@/components/DropCard.vue'
-
+import RealDropCard from '@/components/RealDropCard.vue'
 export default {
   name: 'DropListView',
   components: {
-    DropCard,
+    RealDropCard,
   },
   data() {
     return {
@@ -40,6 +66,74 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.mymargintop {
+  margin-top: 10rem;
+}
+.my_bord_text {
+  width: 481px;
+  height: 63px;
+  margin-left: 16rem;
+  font-family: 'LeferiPoint-BlackA';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 55px;
+  line-height: 63px;
+  display: flex;
+  align-items: center;
+  text-align: center;
 
+  color: #425E7A;
+}
+
+
+/* 긁긁 */
+/* html, body { height: 100%; }
+body {
+  background:radial-gradient(ellipse at center, rgba(255,254,234,1) 0%, rgba(255,254,234,1) 35%, #B7E8EB 100%);
+  overflow: hidden;
+} */
+
+.ocean { 
+  height: 5%;
+  width:100%;
+  position:absolute;
+  bottom:0;
+  left:0;
+  background: #015871;
+}
+
+.wave {
+  background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x; 
+  position: absolute;
+  top: -198px;
+  width: 6400px;
+  height: 198px;
+  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+  transform: translate3d(0, 0, 0);
+}
+
+.wave:nth-of-type(2) {
+  top: -175px;
+  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+  opacity: 1;
+}
+
+@keyframes wave {
+  0% {
+    margin-left: 0;
+  }
+  100% {
+    margin-left: -1600px;
+  }
+}
+
+@keyframes swell {
+  0%, 100% {
+    transform: translate3d(0,-25px,0);
+  }
+  50% {
+    transform: translate3d(0,5px,0);
+  }
+}
 </style>
